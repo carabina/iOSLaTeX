f3a32502a30fc90b2e2d757496516eb9ea0963c7
Shuaib Jewon <shuaib.jewon@slader.com>
2018-07-23 19:29:03 -0400

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-iOSLaTeX_Tests/Pods-iOSLaTeX_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSLaTeX_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSLaTeX_Tests
+@end
---
+++ Example/Pods/Target Support Files/iOSLaTeX/iOSLaTeX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSLaTeX : NSObject
+@end
+@implementation PodsDummy_iOSLaTeX
+@end
