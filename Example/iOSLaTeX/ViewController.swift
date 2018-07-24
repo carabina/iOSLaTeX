eb7c0878409edad74a1b454ea2d2012279c6d130
Shuaib Jewon <shuaib.jewon@slader.com>
2018-07-24 11:12:58 -0400

    Adding example LaTeX array

--- Example/iOSLaTeX/ViewController.swift
+++ Example/iOSLaTeX/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var laTeXImageView: LaTeXImageView!
     @IBOutlet weak var textfield: UITextField!
     
+    let exampleLaTeXArray = [
+        "$$(a_1 + a_2)^2 = a_1^2 + 2a_1a_2 + a_2^2$$",
+        "$$x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}$$",
+        "$$\\sigma = \\sqrt{\\frac{1}{N}\\sum_{i=1}^N (x_i - \\mu)^2}$$",
+        "$$\\neg(P\\land Q) \\iff (\\neg P)\\lor(\\neg Q)$$",
+        "$$\\log_b(x) = \\frac{\\log_a(x)}{\\log_a(b)}$$",
+        "$$\\cos(\\theta + \\varphi) = \\cos(\\theta)\\cos(\\varphi) - \\sin(\\theta)\\sin(\\varphi)$$"
+    ]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -32,37 +41,14 @@ class ViewController: UIViewController {
     
     @IBAction func generateLaTeXButtonTapped(_ sender: Any) {
         let textfieldText = textfield.text
-        let defaultLaTeXText = """
-        [math]y=4x[/math]
         
-        [math]3x-y=1[/math]
-        
-        Now plug 4x into y.
-        
-        [math]3x-4x=1[/math]
-        
-        Subtract.
-        
-        [math]-x=1[/math]
-        
-        Divide/multiply by -1.
-        
-        [math]\\dfrac{-x}{-1}=\\dfrac{1}{-1}[/math]
-        
-        [math]x=-1[/math]
-        
-        If you're solving for y as well, plug -1 into y=4x.
-        
-        [math]y=4x[/math]
-        
-        [math]y=4(-1)[/math]
-        
-        [math]y=-4[/math]
-        """
-        
-        if let laTeX = textfieldText?.trimmingCharacters(in: .whitespaces), !laTeX.isEmpty {
+        if let laTeX = textfieldText?.trimmingCharacters(in: .whitespaces), !laTeX.isEmpty, !exampleLaTeXArray.contains(laTeX) {
             renderLaTeX(laTeX)
         } else {
+            let randomNum:UInt32 = arc4random_uniform(UInt32(exampleLaTeXArray.count))
+            let defaultLaTeXText = exampleLaTeXArray[Int(randomNum)]
+            
+            textfield.text = defaultLaTeXText
             renderLaTeX(defaultLaTeXText)
         }
     }
